6d864d3753659f747a4b9f84aa3e907960015ca8
faridun-dev <faridunf200630@gmail.com>
2024-09-10 22:16:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cats_facts_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cats_facts_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cats_facts_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
